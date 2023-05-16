7b2d83c837b2f0ced1589b6f463bf9afeb60ef7a
NhlakaniphoSingular <nmsutwana@singular.ac.za>
2023-05-15 23:46:37 -0700

    Fixinng betty

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -15,11 +15,9 @@ binary_tree_t *new;
 new = malloc(sizeof(binary_tree_t));
 if (new == NULL)
 return (NULL);
-
 new->n = value;
 new->parent = parent;
 new->left = NULL;
 new->right = NULL;
-
- return (new);
+return (new);
 }
